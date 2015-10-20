b093ee06e693704e457d46f0fe222c1e92b26e2d
Guilherme Toti <guilhermetoti@gmail.com>
2015-10-20 12:16:36 -0200

    added html templates and configs to the application

---
+++ app/Settings.py
@@ -0,0 +1,6 @@
+import os
+
+dirname = os.path.dirname(__file__)
+
+STATIC_PATH = os.path.join(dirname, 'website/static')
+TEMPLATE_PATH = os.path.join(dirname, 'website/views')
--- app/main.py
+++ app/main.py
@@ -1,15 +1,34 @@
+import Settings
 import tornado.ioloop
 import tornado.web
+import tornado.httpserver
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler)
+        ]
+
+        settings = {
+            "template_path": Settings.TEMPLATE_PATH,
+            "static_path": Settings.STATIC_PATH,
+        }
+
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("from server 1")
+        self.render("index.html")
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+
+def main():
+    application = Application()
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(9000)
+
+    tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
-    application.listen(9000)
-    tornado.ioloop.IOLoop.current().start()
+    main()
